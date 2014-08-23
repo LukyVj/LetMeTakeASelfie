fa1abf67316128f7efc75dabb96af9ed3f08da49
LukyVj <lucas.bonomi@gmail.com>
2014-08-22 17:32:13 -0700

    Site created

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,62 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+
+// Define vars 
+var $target = $("#let-me-take-a-selfie ul");
+var $introTime = 5000;
+
+function displayMusic(){
+  var ctx = $('.site-wrapper');
+  ctx.prepend('<audio autoplay><source src="audio/selfie_chainsmoker.ogg" type="audio/ogg"><source src="audio/selfie_chainsmoker.mp3" type="audio/mpeg">Your browser does not support the audio tag.</audio>')
+}
+
+function appendSelfies(){
+  
+  setTimeout(function(){
+    displayMusic();
+  }, $introTime/2)
+  setTimeout(function(){
+       $.ajax({
+    type: "GET",
+    dataType: "jsonp",
+    cache: false,
+    url: "https://api.instagram.com/v1/tags/selfie/media/recent?access_token=52854534.5b9e1e6.cfde91dfcf3c4da5b2744237ccf9264a",
+    success: function(data) {
+      for (var i = 0; i < 20; i++) {
+        $target.append("<li class='pop'><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a></li>");
+      }
+    }
+  });
+
+
+  }, $introTime)
+
+}
+
+function RefreshSelfies(){
+   setInterval(function(){
+
+    $.ajax({
+  type: "GET",
+  dataType: "jsonp",
+  cache: false,
+  url: "https://api.instagram.com/v1/tags/selfie/media/recent?access_token=52854534.5b9e1e6.cfde91dfcf3c4da5b2744237ccf9264a",
+  success: function(data) {
+    for (var i = 0; i < 20; i++) {
+      $target.prepend("<li class='pop'><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a></li>");
+    }
+  }
+});
+
+  }, 3000)
+}
+
+
+function deploy(){
+  appendSelfies();
+  RefreshSelfies();
+}
+
+deploy();
+
+});
+
