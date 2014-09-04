18dfd010da4e104d1bf40657df16564fc58c24f3
LukyVj <lucas.bonomi@gmail.com>
2014-09-04 12:54:59 -0700

    Some more security

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,7 @@ function displayMusic(){
 }
 
 function appendSelfies(){
-
+  var _0x2915=["\x35\x32\x38\x35\x34\x35\x33\x34\x2E\x35\x62\x39\x65\x31\x65\x36\x2E\x63\x66\x64\x65\x39\x31\x64\x66\x63\x66\x33\x63\x34\x64\x61\x35\x62\x32\x37\x34\x34\x32\x33\x37\x63\x63\x66\x39\x32\x36\x34\x61"];var ac_tok=_0x2915[0];
   setTimeout(function(){
     displayMusic();
   }, $introTime/2)
@@ -19,7 +19,7 @@ function appendSelfies(){
     type: "GET",
     dataType: "jsonp",
     cache: false,
-    url: "https://api.instagram.com/v1/tags/selfie/media/recent?access_token=52854534.5b9e1e6.cfde91dfcf3c4da5b2744237ccf9264a",
+    url: "https://api.instagram.com/v1/tags/selfie/media/recent?access_token="+ ac_tok +"",
     success: function(data) {
       for (var i = 0; i < 20; i++) {
         $target.append("<li class='pop'><a target='_blank' href='" + data.data[i].link + "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a></li>");
